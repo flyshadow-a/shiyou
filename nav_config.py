5eccd282e2499b816d04a499a787ed908b13cd82
Ziming Zhang <2867346669@qq.com>
2025-12-24 11:43:43 +0800

    更新提交左侧导航配置文件

--- nav_config.py
+++ nav_config.py
@@ -14,13 +14,13 @@ from pages.construction_docs_page import ConstructionDocsPage
 from pages.platform_summary_page import PlatformSummaryPage
 from pages.platform_basic_info_page import PlatformBasicInfoPage
 from pages.platform_structure_info_page import PlatformStructureInfoPage
-from pages.platform_structure_info_page import PlatformStructureInfoPage
 from pages.special_inspection_strategy import SpecialInspectionStrategy
-from pages.model_files_page import ModelFilesPage
-from pages.history_rebuild_files_page import HistoryRebuildFilesPage
-from pages.important_history_rebuild_info_page import ImportantHistoryEventsPage
-from pages.history_inspection_summary_page import HistoryInspectionSummaryPage
-
+from pages.summary_information_table_page import SummaryInformationTablePage
+from pages.platform_load_information_page import PlatformLoadInformationPage
+from pages.upper_block_subproject_calculation_table_page import UpperBlockSubprojectCalculationTablePage
+from pages.platform_strength_page import PlatformStrengthPage
+from pages.feasibility_assessment_page import FeasibilityAssessmentPage
+from pages.feasibility_assessment_results_page import FeasibilityAssessmentResultsPage
 
 NAV_CONFIG = [
     {
@@ -47,9 +47,22 @@ NAV_CONFIG = [
                      },
                 ],
             },
-            {"text": "载荷信息",},
+            {"text": "载荷信息",
+             "children":[
+                 {"text":"汇总信息表","page":SummaryInformationTablePage},
+                 {"text":"平台载荷信息表",
+                  "children":[
+                      {"text":"平台载荷信息","page":PlatformLoadInformationPage},
+                      {"text":"xxx平台上部组块分项目计算表","page":UpperBlockSubprojectCalculationTablePage},
+                  ]}
+             ]},
             {"text": "状态检测（结构和腐蚀性检测）",},
-            {"text": "结构强度/改造可行性评估",},
+            {"text": "结构强度/改造可行性评估",
+             "children":[{"text":"平台强度/改造可行性评估","page":PlatformStrengthPage},
+                         {"text":"WC19-1DPPA平台强度/改造可行性评估","page":FeasibilityAssessmentPage},
+                         {"text": "WC19-1DPPA平台强度/改造可行性评估评估结果", "page": FeasibilityAssessmentResultsPage},
+                         ]},
+
             {"text": "特检策略","page": SpecialInspectionStrategy},
             # 后续可以在这里继续增加其他功能菜单
         ],
@@ -58,10 +71,10 @@ NAV_CONFIG = [
         "text": "文件管理",
         "children": [
             {"text": "建设阶段完工文件","page":ConstructionDocsPage },
-            {"text": "历史改造文件", "page":HistoryRebuildFilesPage},
-            {"text": "模型文件","page":ModelFilesPage},
-            {"text": "重要历史事件记录", "page":ImportantHistoryEventsPage},
-            {"text": "历史检测及结论", "page":HistoryInspectionSummaryPage},
+            {"text": "历史改造文件", },
+            {"text": "模型文件", },
+            {"text": "重要历史事件记录", },
+            {"text": "历史检测及结论", },
         ],
     },
     # 还可以继续增加「文件管理」等大类
